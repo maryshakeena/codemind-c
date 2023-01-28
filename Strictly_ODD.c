ca9b871a934570d83b0d30257a2f6886bd720893
M.Mary Shakeena <119723872+maryshakeena@users.noreply.github.com>
2023-01-28 10:38:19 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     if(a[i]%2!=0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    if(a[i]%2!=0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
