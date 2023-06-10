318f79febc310e502e903f9234650573321af890
M.Mary Shakeena <119723872+maryshakeena@users.noreply.github.com>
2023-06-10 10:04:00 +0530

    #include<stdio.h> int main() {     int a,b,arr[100],i,n,s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             printf("%d ",arr[i]);         }     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             printf("%d ",arr[i]);         }     } }

---
+++ Even_elements_followed_by_odd_elements.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,arr[100],i,n,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+}
+ 
